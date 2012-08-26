42d108c4774a4f834d018f538f40c395fbb9f8ab
Luke DeWitt <ledewitt@gmail.com>
2012-08-26 10:02:30 -0600

    Added some window dressing to the home page working on the layout.

--- simple_blog.rb
+++ simple_blog.rb
@@ -1,8 +1,15 @@
 require "sinatra"
 require "sinatra/reloader" if development?
 
+TITLE    = File.read("./window_dressing/blog_title.txt")
+SUBTITLE = File.read("./window_dressing/blog_subtitle.txt")
+
 get('/') {
-  erb :home
+  # Get each of the articles as in the snippet of code in reading_article.rb
+  # Can you figure out how to link that code?  Would be cool to just call it
+  # form the lib folder and have it fill some kind of container.
+  erb :home, locals: { title:    TITLE,
+                       subtitle: SUBTITLE }
 }
 
 get('/add_article') {
