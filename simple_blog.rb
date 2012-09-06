2ad13e45c9d949f4eb0f6f855f4ba55801872eb3
Luke DeWitt <ledewitt@gmail.com>
2012-09-06 13:16:55 -0600

    Housecleaning of comments and spaces around hyperlink tags.

--- simple_blog.rb
+++ simple_blog.rb
@@ -4,16 +4,10 @@ require "sinatra/reloader" if development?
 require_relative "lib/simple_blog/article_list"
 require_relative "lib/simple_blog/article"
 
-# TODO: Should change the following variables into a hash reading it out of
-# a single file of environment variables rather then a folder.
-
 CONFIG = YAML.load(File.read("blog_config.yml"))
 
 get('/') {
-  # Get each of the articles as in the snippet of code in reading_article.rb
-  # Can you figure out how to link that code?  Would be cool to just call it
-  # form the lib folder and have it fill some kind of container.
-  
+    
   articles = Simple_Blog::ArticleList.new
   
   erb :home, locals: { articles: articles }
